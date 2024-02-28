92530132d019cada90334d89afe474cc275f5e6d
gon_14 <poniemangon@gmail.com>
2024-02-28 13:10:02 -0300

    more content

---
+++ scripts/slider.js
@@ -0,0 +1,32 @@
+$('.carousel').slick({
+  arrows: false,
+  infinite: false,
+  speed: 300,
+  slidesToShow: 4,
+  slidesToScroll: 4,
+  responsive: [{
+      breakpoint: 1250,
+      settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+      }
+  },
+  {
+    breakpoint: 980,
+    settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2,
+
+    }
+},
+  {
+      breakpoint: 480,
+      settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+
+      }
+  }]
+});
+
