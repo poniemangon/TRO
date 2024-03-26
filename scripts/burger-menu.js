615fe1bbe4d5422ad8afcd05c7c243dd7ff9e386
gon_14 <poniemangon@gmail.com>
2024-03-26 14:17:14 -0300

    cart y modal logica

--- scripts/burger-menu.js
+++ scripts/burger-menu.js
@@ -4,18 +4,42 @@ $(document).ready(function() {
     });
   });
 
-  $(document).ready(function() {
+$(document).ready(function() {
     $(".toggle-box-one").click(function() {
     $(".hidden-p-one").slideToggle();
     $(".flip-one").toggleClass("rotated");
     });
   });
 
-  $(document).ready(function() {
+$(document).ready(function() {
     $(".toggle-box-two").click(function() {
     $(".hidden-p-two").slideToggle();
     $(".flip-two").toggleClass("rotated");
     });
   });
+$(document).ready(function() {
+    
+    $(".fa-cart-shopping").click(function() {
+      $(".overlay").toggle();
+      $(".cart").fadeIn().animate({right: '0'}, 300);
+    });
+  
+  $(".close-cart").click(function() {
+      $(".overlay").fadeOut();
+      $(".cart").animate({right: '-800px'}, 600).fadeOut();
+    });
+  });
+  
 
-
+  $(document).ready(function(){
+    
+    $(".fa-plus").click(function() {
+      $(".overlay").toggle();
+      $(".modal-payment").fadeIn();
+    });
+  
+  $(".close-modal").click(function() {
+      $(".overlay").fadeOut();
+      $(".modal-payment").fadeOut();
+    });
+  });
